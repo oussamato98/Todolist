348544af3b8b08bed646d85a76b3e7ea30dcff9b
oussamato98 <oussamaaouad98@gmail.com>
2023-05-17 16:56:56 +0100

    Passing Data from your webpage to your server

--- app.js
+++ app.js
@@ -2,10 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+var items =["Buy Food","Cook Food","Eat Food"];
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine', 'ejs');
+
+
 app.listen(3000,function(){
     console.log("Server is running at 3000");
 });
 
 app.get("/",function(req,res){
-    res.send("Hello");
+
+
+/*     https://stackoverflow.com/questions/3552461/how-do-i-format-a-date-in-javascript
+ */    
+    
+    var options = { weekday: 'long', month: 'long', day: 'numeric' };
+    var today = new Date();
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("index",{kindOfDay : day , kindOfInput : items})
 });
+
+app.post("/",function(req,res){
+
+
+    var input = req.body.input ;
+    items.push(input);
+    res.redirect("/");
+
+   // res.send(input);
+
+})
+
+
